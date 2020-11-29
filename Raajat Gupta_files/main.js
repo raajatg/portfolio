d6fdab90365c58c64640411b2e9e18ffba24fbc6
raajatg <raajatg@andrew.cmu.edu>
2020-11-28 19:51:23 -0500

    created cover page and initial animations

---
+++ Raajat Gupta_files/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('.third-button').on('click', function () {
+  
+      $('.animated-icon3').toggleClass('open');
+    });
+  });
+
+
+  //On hover change words
+  var specialWords = document.getElementsByClassName("hoverWord");
+  for(let word of specialWords){
+      word.addEventListener('mouseenter', function(){
+          this.innerHTML = this.getAttribute('data-words').split("|")[1];
+      });
+      word.addEventListener('mouseleave', function(){
+          this.innerHTML = this.getAttribute('data-words').split("|")[0];
+      });
+  }
---
+++ typewriter.js
@@ -0,0 +1,17 @@
+import Typewriter from './core';
+
+var app = document.getElementById('app');
+ 
+const typewriter = new Typewriter(app, {
+    loop: true,
+  });
+
+  typewriter.typeString('Hello World!')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Strings can be removed')
+    .pauseFor(2500)
+    .deleteChars(7)
+    .typeString('<strong>altered!<strong>')
+    .pauseFor(2500)
+    .start();
