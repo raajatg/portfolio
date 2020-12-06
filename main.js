557ea6512124bb0d4c472057036ade6d929ec3ad
raajatg <raajatg@andrew.cmu.edu>
2020-12-05 19:51:02 -0500

    created header section of project page

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 $(document).ready(function () {
+    //Change the style of the navbar button
     $('.third-button').on('click', function () {
   
       $('.animated-icon3').toggleClass('open');
     });
+    //On hover change words
+    var specialWords = document.getElementsByClassName("hoverWord");
+    for(let word of specialWords){
+        word.addEventListener('mouseenter', function(){
+            this.innerHTML = this.getAttribute('data-words').split("|")[1];
+        });
+        word.addEventListener('mouseleave', function(){
+            this.innerHTML = this.getAttribute('data-words').split("|")[0];
+        });
+    }
   });
 
 
-  //On hover change words
-  var specialWords = document.getElementsByClassName("hoverWord");
-  for(let word of specialWords){
-      word.addEventListener('mouseenter', function(){
-          this.innerHTML = this.getAttribute('data-words').split("|")[1];
-      });
-      word.addEventListener('mouseleave', function(){
-          this.innerHTML = this.getAttribute('data-words').split("|")[0];
-      });
-  }
+  
+  
---
+++ type.js
@@ -0,0 +1,23 @@
+var app = document.getElementById('app');
+
+const homeType = new Typewriter(app);
+    homeType.pauseFor(4000)
+    .typeString('Oh, hello there 👋!')
+    .pauseFor(2000)
+    .deleteAll()
+    .typeString("I'm Raajat Gupta.")
+    .start();
+
+var introduction = document.getElementById('introduction');
+
+const meType = new Typewriter(introduction);
+    meType.pauseFor(1000)
+    .typeString("I'm Raajat Gupta 🙋🏽‍♂️.")
+    .typeString('<br/>')
+    .pauseFor(500)
+    .typeString("I use design as a tool 🔧 for building community.")
+    .typeString(" 🏚")
+    .pauseFor(1000)
+    .deleteChars(3)
+    .typeString(" 🏘")
+    .start();
