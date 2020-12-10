1dd79cd22fe31cecba20e350290b231144cb51ac
raajatg <raajatg@andrew.cmu.edu>
2020-12-10 18:22:07 -0500

    embedded type js

--- type.js
+++ type.js
@@ -1,19 +1,19 @@
 var app = document.getElementById('app');
-let handWave = String.fromCodePoint(0x1F44B);;
+let handWave = String.fromCharCode();
 
-const homeType = new Typewriter(app);
-    homeType.pauseFor(4000)
-    .typeString(`Oh, hello there ${handWave}!`)
-    .pauseFor(2000)
-    .deleteAll()
-    .typeString("I'm Raajat Gupta.")
-    .start();
+//const homeType = new Typewriter(app);
+  //  homeType.pauseFor(4000)
+    //.typeString(`Oh, hello there ${handWave}!`)
+    //.pauseFor(2000)
+    //.deleteAll()
+    //.typeString("I'm Raajat Gupta.")
+    //.start();
 
 var introduction = document.getElementById('introduction');
 
 const meType = new Typewriter(introduction);
     meType.pauseFor(1000)
-    .typeString("I'm Raajat Gupta 🙋🏽‍♂️.")
+    .typeString("I'm Raajat Gupta.")
     .typeString('<br/>')
     .pauseFor(500)
     .typeString("I use design as a tool 🔧 for building community.")
